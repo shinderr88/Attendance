5540f11456f4b7fe4358f87cc4de4f258a188509
rahul <shinderr88@gmail.com>
2020-12-15 00:28:16 +0530

    First commit

---
+++ site-attendance/src/App.js
@@ -0,0 +1,32 @@
+import {useDispatch, useSelector} from 'react-redux'
+import {Field, Form, Formik} from 'formik'
+import {login, logout} from './reduxSetup/user'
+function App() {
+  const dispatch = useDispatch()
+  const { user } = useSelector(state => state.user)
+  if (user) {
+    return (
+      <div>
+        Hi, {user.username}!
+        <button onClick={() => dispatch(logout())}>Logout</button>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(values) => { dispatch(login(values)) }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="username" />
+            <Field type="password" name="password" />
+            <button type="submit" disabled={isSubmitting}>Login</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
+export default App
---
+++ site-attendance/src/reduxSetup/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+
+//import the reducers
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
